1ea6b5d0bd722b88ca8b38c8bf9a8954c6f3e1de
Neto test <tanyadima1909@gmail.com>
2026-08-22 11:38:40 +0500

    Configure Node.js development environment

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      "no-debugger": "off",
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "script" },
+  },
+]);
